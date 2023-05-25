34d0b7f97290a25ac9c839705e3f28d94561728e
Aidan-B1409 <aidanb1049@gmail.com>
2023-05-24 18:08:59 -0700

    Ingest data to huggingfaces

--- src/app.py
+++ src/app.py
@@ -1,9 +1,18 @@
 import pickle
 import argparse
 import numpy as np
+import pyarrow as pa
 import pandas as pd
+import datasets
+from datasets import load_dataset
+
+def parseargs():
+    parser = argparse.ArgumentParser(help="")
 
 def main():
+    ds = dataloader()
+
+def dataloader() -> datasets.Dataset:
     objects = []
     with open('dataset/expert/FetchPick/buffer.pkl', 'rb') as expert_fetch_pick:
         while True:
@@ -16,10 +25,14 @@ def main():
     for key in objects[0].keys():
         objects[0][key] = np.asarray(objects[0][key])
         print(objects[0][key].shape)
-    # print(objects[0])
-    # df = pd.DataFrame.from_dict(objects[0])
-    # print(df)
-    # print(df.summary())
+    print(objects[0]['ag'])
+
+    ds = datasets.Dataset.from_dict(objects[0])
+    for old, new in zip(ds.column_names, ['obs', 'actions', 'goal', 'achieved_goal']):
+        ds = ds.rename_column(old, new)
+    print(ds)
+
+    
 
 if __name__ == '__main__':
-    main()
+    dataloader()
