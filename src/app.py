a7f3f26e96ac198b1883225afb5f084398a85eec
Aidan-B1409 <aidanb1049@gmail.com>
2023-06-02 14:03:27 -0700

    Add collator

--- src/app.py
+++ src/app.py
@@ -5,6 +5,10 @@ import pyarrow as pa
 import pandas as pd
 import datasets
 from datasets import load_dataset
+from transformers import Trainer, TrainingArguments
+from decisionTransformerGymDataCollator import DecisionTransformerGymDataCollator
+from model_assembler import TrainableDT
+from transformers import DecisionTransformerConfig
 
 def parseargs():
     parser = argparse.ArgumentParser(help="")
@@ -20,17 +24,58 @@ def dataloader() -> datasets.Dataset:
                 objects.append(pickle.load(expert_fetch_pick))
             except EOFError:
                 break
-    print(objects)
+
     # Convert to np arrays
+    # TODO - Handle funky shape logic in collator for other datasts
     for key in objects[0].keys():
         objects[0][key] = np.asarray(objects[0][key])
-        print(objects[0][key].shape)
-    print(objects[0]['ag'])
+        # print(objects[0][key].shape)
+    # print(objects[0]['ag'])
 
     ds = datasets.Dataset.from_dict(objects[0])
-    for old, new in zip(ds.column_names, ['obs', 'actions', 'goal', 'achieved_goal']):
+    for old, new in zip(ds.column_names, ['observations', 'actions', 'goal', 'achieved_goal']):
         ds = ds.rename_column(old, new)
-    print(ds)
+
+    goals = np.asarray(ds['goal'])
+    goals = np.hstack((goals, goals[:, -1:, :]))
+
+    rewards = np.linalg.norm((np.asarray(ds['achieved_goal']) - goals), axis=2, ord=2)
+    dones = np.where(rewards < 0.05, 1, 0)
+
+    ds = ds.add_column('rewards', rewards.tolist())
+    ds = ds.add_column('dones', dones.tolist())
+    # ds['rewards'] = rewards
+    # ds['dones'] = dones
+
+    collator = DecisionTransformerGymDataCollator(ds)
+
+    # TODO - manually adjust state dim, act_dim
+    config = DecisionTransformerConfig(state_dim=collator.state_dim, act_dim=collator.act_dim)
+    model = TrainableDT(config)
+
+
+    
+    training_args = TrainingArguments(
+        output_dir="output/",
+        remove_unused_columns=False,
+        num_train_epochs=120,
+        per_device_train_batch_size=64,
+        learning_rate=1e-4,
+        weight_decay=1e-4,
+        warmup_ratio=0.1,
+        optim="adamw_torch",
+        max_grad_norm=0.25,
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=ds,
+        data_collator=collator,
+    )
+
+    trainer.train()
+
 
 if __name__ == '__main__':
     dataloader()
