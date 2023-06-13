dd146f4e3af64ed87bc9ea0f3640d29623997bc4
Aidan-B1409 <aidanb1049@gmail.com>
2023-06-12 20:39:53 -0700

    Debug Prints

--- src/main/app.py
+++ src/main/app.py
@@ -75,8 +75,8 @@ def dataloader(path: str, p: float, args: argparse.Namespace) -> datasets.Datase
     ds = ds.add_column('observations', states.tolist())
      # TODO - Calculate target return
     avg_target_return = np.mean(np.sum(rewards, axis=1), axis=0)
-    print(f"Average Target Return: {np.asarray(avg_target_return)}")
-    print(f"Action Shape: {ds['actions'].shape}")
+    print(f"Average Target Return: {avg_target_return}")
+    print(f"Action Shape: {np.asarray(ds['actions']).shape}")
     return ds
 
 def main():
